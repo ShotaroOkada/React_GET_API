ee7889b5fc015fc803e82b4e086d6356f419491e
OkadaShotaro <b1016117@fun.ac.jp>
2019-04-06 14:43:16 +0900

    郵便番号を入力すると住所が返ってくる機能を実装

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,22 @@ import './App.css';
 
 import logo from './logo.svg';
 
-class App extends React.Component {
+export interface IAppState {
+  zipcode: string;
+  address: string;
+}
+
+class App extends React.Component<{}, IAppState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      address: '',
+      zipcode: ''      
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
   public render() {
     return (
       <div className="App">
@@ -11,11 +26,32 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <form onSubmit={this.handleSubmit}>
+          <p className="App-intro">
+            <input type="text" value={this.state.zipcode} onChange={this.handleChange} />
+            <input type="submit" value="検索" />
+          </p>
+        </form>
+        <p>{this.state.address}</p>
       </div>
-    );
+    ); 
+  }
+
+  public handleChange(e: any) {
+    this.setState({ zipcode: e.target.value });
+  }
+
+  public handleSubmit(e: any) {
+    fetch(`https://api.zipaddress.net/?zipcode=${this.state.zipcode}`, {
+      mode: 'cors'
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((myJson) => {
+        this.setState({ address: myJson.data.fullAddress });
+      });
+    e.preventDefault();
   }
 }
 
